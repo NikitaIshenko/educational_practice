d0d2c12758328fd5074c3c48959cfa8e89878a0d
LAPTOP-4Q0DUPQ9\ROG <IshenkoNikita905@mail.ru>
2023-05-15 11:21:55 +0300

    add read header

--- src/main/java/com/city/building/controller/InfoController.java
+++ src/main/java/com/city/building/controller/InfoController.java
@@ -2,11 +2,14 @@ package com.city.building.controller;
 
 import com.city.building.models.MathExpectation;
 import com.city.building.models.CityList;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("info")
@@ -14,14 +17,17 @@ public class InfoController {
     CityList cityList = new CityList();
     MathExpectation mathExpectation = new MathExpectation();
 
+
     @GetMapping
-    public HashMap<String, Object> info(){
-        HashMap<String, Object> info = new HashMap<String, Object>();
+    public LinkedHashMap<String, Object> info(HttpServletRequest request){
+        LinkedHashMap<String, Object> info = new LinkedHashMap<String, Object>();
         cityList.readXML();
         int count_building = cityList.listCity().size();
         double math_wait = mathExpectation.matAnalyze(cityList);
+        String username = request.getHeader("username");
 
-
+        if(!Objects.equals(username, null))
+            info.put("Приветствие", "Привет, " + username);
         info.put("ФИО автора", "Ищенко Никита Александрович");
         info.put("Вариант", 16);
         info.put("Предметная область", "Город");
